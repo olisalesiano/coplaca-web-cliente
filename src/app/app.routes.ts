6badd74a34ca76b421104f044dd93b937aeb2f62
Olihdezp <oligamer112@gmail.com>
2026-03-05 10:11:02 +0000

    login-cliente:"Primeros estilos del login"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,15 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ourProductsComponent } from './components/our-products/our-products.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { CartComponent } from './components/cart/cart.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'register', component: RegisterComponent },
+  { path: 'our-products', component: ourProductsComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'cart', component: CartComponent },
 ];
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,17 +1,28 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatIcon],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  constructor(private router: Router) {}
   showPassword = false;
 
   togglePassword(): void {
     this.showPassword = !this.showPassword;
+  }
+
+  goToOurProducts(): void {
+    this.router.navigate(['/our-products']);
+  }
+
+  goToRegister(): void {
+    this.router.navigate(['/register']);
   }
 }
